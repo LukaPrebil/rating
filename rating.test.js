53462d2973936416c8cd138d66024923dbc7346f
Luka Prebil Grintal <luka.p.grintal@tretton37.com>
2022-06-10 17:49:51 +0200

    Add configurable symbols

--- rating.test.js
+++ rating.test.js
@@ -11,4 +11,20 @@ describe(`${rating.name} tests`, () => {
   it("Should allow configured max value", () => {
     expect(rating(5, { max: 10 })).toMatchSnapshot();
   })
+
+  it("Should allow configured success symbol", () => {
+    expect(rating(2, { symbolSuccess: "x" })).toMatchSnapshot();
+  })
+
+  it("Should allow configured fail symbol", () => {
+    expect(rating(3, { symbolFail: "o" })).toMatchSnapshot();
+  })
+
+  it("Should allow configured both symbols", () => {
+    expect(rating(4, { symbolSuccess: "x", symbolFail: "o" })).toMatchSnapshot();
+  })
+
+  it("Should allow configured both symbols and max value", () => {
+    expect(rating(5, { symbolSuccess: "x", symbolFail: "o", max: 10 })).toMatchSnapshot();
+  })
 });
